24128a4a6f92920b19ad30b4cd33c22d4d24722e
arizusmani007 <arizusmani007@gmail.com>
2021-09-21 13:13:43 +0530

    change

--- CRS-LT-POS-Development/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.lt.bean"},{"l":"com.lt.business"},{"l":"com.lt.client"},{"l":"com.lt.constants"},{"l":"com.lt.dao"},{"l":"com.lt.exception"},{"l":"com.lt.utils"},{"l":"com.lt.validator"}];updateSearchResults();
