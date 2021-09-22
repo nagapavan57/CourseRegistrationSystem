03617b41a5ac6877677e0925ba958c8fd01b69b4
arizusmani007 <arizusmani007@gmail.com>
2021-09-22 13:33:35 +0530

    exception class for professor
    added "fee" field in Course class

--- CRS-LT-REST-Development/src/main/java/com/lt/exception/GradeNotAddedException.java
+++ CRS-LT-REST-Development/src/main/java/com/lt/exception/GradeNotAddedException.java
@@ -2,14 +2,13 @@ package com.lt.exception;
 
 public class GradeNotAddedException extends Exception {
 
-public int studentId;
-	
-	public GradeNotAddedException(int studentId)
-	{	
+	public int studentId;
+
+	public GradeNotAddedException(int studentId) {
 		this.studentId = studentId;
 	}
-	
-	public String getMessage(int studentId){
-		return "Grade Assigning Failed for Student with Id:"+studentId;
+
+	public String getMessage(int studentId) {
+		return "Grade Assigning Failed for Student with Id:" + studentId;
 	}
 }
